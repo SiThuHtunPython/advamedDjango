81a90401c2603b29419de231d429b0de87bfb8a1
Si Thu Htun <chekoilar@gmail.com>
2021-10-21 21:52:26 +0630

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'cb141294dece657863ac4352dd4a39e33468b26cce15fec0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['advamedwork.herokuapp.com']
 
 
 # Application definition
