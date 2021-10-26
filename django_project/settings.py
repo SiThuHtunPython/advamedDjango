dd544f70974df7b33d8418ca323b428a3c5537c8
Si Thu Htun <chekoilar@gmail.com>
2021-10-26 16:39:04 +0630

    Updated requirement.txt

--- django_project/settings.py
+++ django_project/settings.py
@@ -161,4 +161,4 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
